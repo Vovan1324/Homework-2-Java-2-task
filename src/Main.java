2528a573f1b5326ee80b141a04ffb486a195c439
Vladimir <zaicev.94@mail.ru>
2022-11-07 19:21:49 +0300

    Homework 2 java 2 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int accountclient = 100;
+        int depositamount = 1100;
+        int bonus;
+
+        if (depositamount > 1000) {
+            bonus = depositamount / 100;
+        }
+        else { bonus = 0;
+        }
+
+        int balanceaccount = accountclient + depositamount + bonus;
+        System.out.println("Баланс вашего счета:" + balanceaccount + " руб. Полученный бонус: " + bonus + "руб" );
     }
 }
